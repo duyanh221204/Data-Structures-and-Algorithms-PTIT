fb63d287a38028c1df65a3e8977845c9d28f6fcd
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-09 23:01:17 +0700

    Create DSA05013 - Bậc thang.cpp

---
+++ Dynamic Programming/DSA05013 - Bậc thang.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int n, k, mod = 1e9 + 7;
+        cin >> n >> k;
+        long long f[n + 2] = {};
+        f[0] = f[1] = 1;
+        for (int i = 2; i <= n; ++i)
+        {
+            for (int j = 1; j <= min(i, k); ++j) (f[i] += f[i - j]) %= mod;
+        }
+        cout << f[n] << "\n";
+    }
+}
