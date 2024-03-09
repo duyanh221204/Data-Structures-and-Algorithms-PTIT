719fd574f7f7c30eb2fe4df620f821ca3dc3c07e
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-09 23:05:26 +0700

    Create DSAKT055 - Bài toán cái túi 0-1.cpp

---
+++ Dynamic Programming/DSAKT055 - Bài toán cái túi 0-1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int n, v;
+        cin >> n >> v;
+        int a[n + 2], c[n + 2], f[n + 2][v + 2] = {};
+        for (int i = 1; i <= n; ++i) cin >> a[i];
+        for (int i = 1; i <= n; ++i) cin >> c[i];
+        for (int i = 1; i <= n; ++i)
+        {
+            for (int j = 1; j <= v; ++j)
+            {
+                if (a[i] > j) f[i][j] = f[i - 1][j];
+                else f[i][j] = max(f[i - 1][j], f[i - 1][j - a[i]] + c[i]);
+            }
+        }
+        cout << f[n][v] << "\n";
+    }
+}
