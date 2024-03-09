2fe1d981ef78e6a126fc2777b288269bfb74a792
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-09 22:41:25 +0700

    Create DSA04002 - Lũy thừa đảo.cpp

---
+++ Divide and Conquer/DSA04002 - Lũy thừa đảo.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int mod = 1e9 + 7;
+long long so(long long n)
+{
+    long long d = 0;
+    while (n)
+    {
+        (d *= 10) += (n % 10);
+        n /= 10;
+    }
+    return d;
+}
+long long tt(long long n, long long k)
+{
+    if (!k) return 1;
+    if (k == 1) return n;
+    long long s = tt(n, k >> 1) % mod;
+    (s *= s) %= mod;
+    if (k & 1) (s *= n) %= mod;
+    return s;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        long long n, r;
+        cin >> n;
+        r = so(n);
+        cout << tt(n, r) << "\n";
+    }
+}
