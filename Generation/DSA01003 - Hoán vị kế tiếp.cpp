444b91337af37720888fdddfb642fa80b33e439d
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-10 09:53:07 +0700

    Create DSA01003 - Hoán vị kế tiếp.cpp

---
+++ Generation/DSA01003 - Hoán vị kế tiếp.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, d, a[1002];
+void tt()
+{
+    int x = n - 1, y = n, l, r;
+    while (x && a[x] > a[x + 1]) --x;
+    if (!x)
+    {
+        d = 1;
+        return;
+    }
+    while (y && a[x] > a[y]) --y;
+    if (!y)
+    {
+        d = 1;
+        return;
+    }
+    swap(a[x], a[y]);
+    l = x + 1;
+    r = n;
+    while (l < r) swap(a[l++], a[r--]);
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        cin >> n;
+        for (int i = 1; i <= n; ++i) cin >> a[i];
+        tt();
+        if (d)
+        {
+            for (int i = n; i; --i) cout << i << " ";
+        }
+        else
+        {
+            for (int i = 1; i <= n; ++i) cout << a[i] << " ";
+        }
+        cout << "\n";
+        d = 0;
+    }
+}
