f6d4c7629d5ec0371f41ab9f040a22db97ef3fff
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-10 11:20:18 +0700

    Create DSA07010 - Biến đổi tiền tố-hậu tố.cpp

---
+++ Stack/DSA07010 - Biến đổi tiền tố-hậu tố.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        string s;
+        cin >> s;
+        stack<string> st;
+        for (int i = s.size() - 1; ~i; --i)
+        {
+            if (s[i] == 42 || s[i] == 43 || s[i] == 45 || s[i] == 47)
+            {
+                string k1 = st.top();
+                st.pop();
+                string k2 = st.top();
+                st.pop();
+                st.push(k1 + k2 + s[i]);
+            }
+            else st.push(string(1, s[i]));
+        }
+        cout << st.top() << "\n";
+    }
+}
