11897983b2f3222cb7efb6da937c1846d5129d80
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-10 15:12:18 +0700

    Create DSA08017 - Số lộc phát 1.cpp

---
+++ Queue, Deque, Priority Queue/DSA08017 - Số lộc phát 1.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int tt(long long n)
+{
+    int d = 0;
+    while (n)
+    {
+        ++d;
+        n /= 10;
+    }
+    return d;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int n, d;
+        cin >> n;
+        vector<long long> v;
+        queue<long long> q;
+        q.push(6);
+        q.push(8);
+        while (tt(q.front()) <= n)
+        {
+            long long x = q.front();
+            q.pop();
+            v.push_back(x);
+            q.push(x * 10 + 6);
+            q.push(x * 10 + 8);
+        }
+        d = v.size() - 1;
+        for (int i = d; ~i; --i) cout << v[i] << " ";
+        cout << "\n";
+    }
+}
