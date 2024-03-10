fcad6595f85731889c59adcce7c35478b90a357d
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-10 15:30:49 +0700

    Create DSA09001 - Chuyển danh sách cạnh sang danh sách kề.cpp

---
+++ Graph/Graph Traversal/DSA09001 - Chuyển danh sách cạnh sang danh sách kề.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int v, e;
+        cin >> v >> e;
+        vector<vector<int>> g(v + 2);
+        while (e--)
+        {
+            int x, y;
+            cin >> x >> y;
+            g[x].push_back(y);
+            g[y].push_back(x);
+        }
+        for (int i = 1; i <= v; ++i)
+        {
+            sort(g[i].begin(), g[i].end());
+            cout << i << ": ";
+            for (int& j : g[i]) cout << j << " ";
+            cout << "\n";
+        }
+    }
+}
