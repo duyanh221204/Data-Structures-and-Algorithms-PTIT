b7e5d03c4f5ee861e2b3a315f7e506273483cae4
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-10 15:36:46 +0700

    Create DSA09022 - DFS trên đồ thị có hướng.cpp

---
+++ Graph/Graph Traversal/DSA09022 - DFS trên đồ thị có hướng.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> g(1002);
+int k[1002];
+void tt(int x)
+{
+    cout << x << " ";
+    k[x] = 1;
+    for (int& i : g[x])
+    {
+        if (!k[i]) tt(i);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int v, e, u;
+        cin >> v >> e >> u;
+        while (e--)
+        {
+            int x, y;
+            cin >> x >> y;
+            g[x].push_back(y);
+        }
+        tt(u);
+        cout << "\n";
+        for (int i = 1; i <= v; ++i)
+        {
+            k[i] = 0;
+            g[i].clear();
+        }
+    }
+}
