565866787e525529e889046069b02609e2455125
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-10 10:55:50 +0700

    Create DSA06024 - Sắp xếp chọn.cpp

---
+++ Sort and Search/DSA06024 - Sắp xếp chọn.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    //cin >> T;
+    while (T--)
+    {
+        int n, d = 0;
+        cin >> n;
+        int a[n];
+        for (int& i : a) cin >> i;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            int x = i;
+            for (int j = i + 1; j < n; ++j)
+            {
+                if (a[j] < a[x]) x = j;
+            }
+            swap(a[x], a[i]);
+            cout << "Buoc " << ++d << ": ";
+            for (int& j : a) cout << j << " ";
+            cout << "\n";
+        }
+    }
+}
