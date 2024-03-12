dc93900c4bb80e824d6e2561b69bf9bbaa7ef924
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-03-12 09:56:41 +0700

    Create DSA05043 - Số thuận nghịch.cpp

---
+++ Dynamic Programming/DSA05043 - Số thuận nghịch.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        string s, k;
+        cin >> s;
+        int d = 0, x = s.size(), f[s.size() + 2][s.size() + 2] = {};
+        k = s;
+        reverse(k.begin(), k.end());
+        for (int i = 1; i <= x; ++i)
+        {
+            for (int j = 1; j <= x; ++j)
+            {
+                if (s[i - 1] == k[j - 1])
+                {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                    d = max(f[i][j], d);
+                }
+            }
+        }
+        cout << d << "\n";
+    }
+}
