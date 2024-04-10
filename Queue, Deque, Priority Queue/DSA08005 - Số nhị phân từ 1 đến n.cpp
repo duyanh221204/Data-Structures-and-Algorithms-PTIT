69fb3d8cdccf3419307b19a9eee6a99c85f8a434
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-04-10 21:55:55 +0700

    Create DSA08005 - Số nhị phân từ 1 đến n.cpp

--- Queue, Deque, Priority Queue/DSA08005 - Số nhị phân từ 1 đến n.cpp
+++ Queue, Deque, Priority Queue/DSA08005 - Số nhị phân từ 1 đến n.cpp
@@ -6,29 +6,20 @@ int main()
     cin.tie(0);
     int T = 1;
     cin >> T;
-    vector<string> v;
-    for (int i = 1; i <= 10000; ++i)
-    {
-        int x = i;
-        stack<int> st;
-        while (x)
-        {
-            st.push(x & 1);
-            x >>= 1;
-        }
-        string s = "";
-        while (st.size())
-        {
-            s += (st.top() + 48);
-            st.pop();
-        }
-        v.push_back(s);
-    }
     while (T--)
     {
         int n;
         cin >> n;
-        for (int i = 0; i < n; ++i) cout << v[i] << " ";
+        queue<long long> q;
+        q.push(1);
+        while (n--)
+        {
+            long long m = q.front();
+            q.pop();
+            cout << m << " ";
+            q.push(m * 10);
+            q.push(m * 10 + 1);
+        }
         cout << "\n";
     }
 }
