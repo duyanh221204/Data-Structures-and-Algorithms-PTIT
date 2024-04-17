8e7f91939af6ca15a3a3fd4056705de68a29a60e
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-04-17 09:53:49 +0700

    Create DSA11011 - Cây nhị phân đủ.cpp

---
+++ Tree/DSA11011 - Cây nhị phân đủ.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node* l, *r;
+    node (int data)
+    {
+        this->data = data;
+        this->l = this->r = 0;
+    }
+};
+void tree(node*& head, int par, int child, char dir)
+{
+    if (head)
+    {
+        if (head->data == par)
+        {
+            if (dir == 76) head->l = new node(child);
+            else head->r = new node(child);
+        }
+        tree(head->l, par, child, dir);
+        tree(head->r, par, child, dir);
+    }
+}
+int kt(node* head)
+{
+    if (!head->l && !head->r) return 1;
+    if (!head->l || !head->r) return 0;
+    return kt(head->l) && kt(head->r);
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        node* head = 0;
+        while (n--)
+        {
+            int par, child;
+            char dir;
+            cin >> par >> child >> dir;
+            if (!head) head = new node(par);
+            tree(head, par, child, dir);
+        }
+        cout << kt(head) << "\n";
+    }
+}
