64adf99efc7714dab0d2bb3bb2fe8f16d5bce052
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-04-19 15:26:47 +0700

    Create DSA11018 - Xây dựng lại cây nhị phân tìm kiếm.cpp

---
+++ Tree/DSA11018 - Xây dựng lại cây nhị phân tìm kiếm.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node* l, *r;
+    node (int data)
+    {
+        this->data = data;
+        this->l = this->r = 0;
+    }
+};
+void tree(node*& head, int x)
+{
+    if (!head) head = new node(x);
+    else
+    {
+        if (x < head->data) tree(head->l, x);
+        else tree(head->r, x);
+    }
+}
+void tt(node* head)
+{
+    if (head)
+    {
+        cout << head->data << " ";
+        tt(head->l);
+        tt(head->r);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        node* head = 0;
+        while (n--)
+        {
+            int x;
+            cin >> x;
+            tree(head, x);
+        }
+        tt(head);
+        cout << "\n";
+    }
+}
