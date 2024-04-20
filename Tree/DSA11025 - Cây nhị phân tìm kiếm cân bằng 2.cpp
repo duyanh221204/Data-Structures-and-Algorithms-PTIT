3205488e2b1d08c8463f0d372490ad97c1193e74
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-04-20 21:19:31 +0700

    Create DSA11025 - Cây nhị phân tìm kiếm cân bằng 2.cpp

---
+++ Tree/DSA11025 - Cây nhị phân tìm kiếm cân bằng 2.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[1000002];
+struct node
+{
+    int data;
+    node* l, *r;
+    node (int data)
+    {
+        this->data = data;
+        this->l = this->r = 0;
+    }
+};
+void tree(node*& head, int l, int r)
+{
+    if (l <= r)
+    {
+        int mid = l + r >> 1;
+        head = new node(a[mid]);
+        tree(head->l, l, mid - 1);
+        tree(head->r, mid + 1, r);
+    }
+}
+void tt(node* head)
+{
+    if (head)
+    {
+        cout << head->data << " ";
+        tt(head->l);
+        tt(head->r);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; ++i) cin >> a[i];
+        sort(a + 1, a + n + 1);
+        node* head = 0;
+        tree(head, 1, n);
+        tt(head);
+        cout << "\n";
+    }
+}
