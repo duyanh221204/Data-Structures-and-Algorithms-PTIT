f3da1c94e5023df1c43d9fdc89744afe8a169359
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-04-22 11:04:51 +0700

    Create DSA06038 - Cặp số.cpp

--- Sort and Search/DSA06038 - Cặp số.cpp
+++ Sort and Search/DSA06038 - Cặp số.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n;
-long long a[100002], t[1000002];
+long long m, a[100002], t[1000002];
 void update(long long x)
 {
-    while (x <= n)
+    while (x <= m)
     {
         ++t[x];
         x += (x & -x);
@@ -29,7 +29,11 @@ int main()
     while (T--)
     {
         cin >> n;
-        for (int i = 1; i <= n; ++i) cin >> a[i];
+        for (int i = 1; i <= n; ++i)
+        {
+            cin >> a[i];
+            m = max(a[i], m);
+        }
         queue<long long> q;
         long long d = 0;
         for (int i = n; i; --i)
