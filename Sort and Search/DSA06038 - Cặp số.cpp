bea7f8408f4384c401a9501a4f64dfa7dbda47d6
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-04-22 10:59:38 +0700

    Create DSA06038 - Cặp số.cpp

---
+++ Sort and Search/DSA06038 - Cặp số.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+long long a[100002], t[1000002];
+void update(long long x)
+{
+    while (x <= n)
+    {
+        ++t[x];
+        x += (x & -x);
+    }
+}
+long long tt(long long x)
+{
+    long long d = 0;
+    while (x >= 1)
+    {
+        d += t[x];
+        x -= (x & -x);
+    }
+    return d;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T = 1;
+    //cin >> T;
+    while (T--)
+    {
+        cin >> n;
+        for (int i = 1; i <= n; ++i) cin >> a[i];
+        queue<long long> q;
+        long long d = 0;
+        for (int i = n; i; --i)
+        {
+            if (a[i] & 1)
+            {
+                while (q.size())
+                {
+                    update(q.front());
+                    q.pop();
+                }
+            }
+            else
+            {
+                d += tt(a[i] - 1);
+                q.push(a[i]);
+            }
+        }
+        cout << d;
+    }
+}
